69fe5fadae16b64d91efa73712371dd5f6d7b6b9
unknown <kaja.ricna@seznam.cz>
2021-01-12 11:36:18 +0100

    update

--- js.js
+++ js.js
@@ -1,5 +1,46 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+
+
+class Enemy {
+    constructor(x, y, a,b){
+        this.x = x
+        this.y = y
+        this.a = a 
+        this.b = b 
+
+    }
+     paint (){
+
+            ctx.fillStyle = 'red';
+            ctx.fillRect(this.x, this.y, this.a, this.b);
+
+     }
+}
+
+let enemy1 = new Enemy(10, 10, 100, 50)
+
+let enemies = []
+
+function pridejEnemy(){
+    let pomoc = 10;
+    for ( i = 0; i <= 9 ;i++ ){
+  enemies.push(new Enemy(pomoc, 10, 100, 50))
+  enemies[i].paint()
+ pomoc +=110;
+    }
+
+}
+
+class Bullet {
+    constructor(x, y, a,b){
+        this.x = x
+        this.y = y
+        this.a = a 
+        this.b = b 
+    }
+}
+
 //*******************************************
 let strelec = {
     x: canvas.width / 2,
@@ -14,98 +55,13 @@ let strelec = {
     }
 
 }
-// ********************************************
-let enemy = {
-    xe: 10,
-    ye: 10,
-    ae: 100,
-    be: 50,
-    mezera: 10,
-    pocet: 9,
-    paint: function(){
-        ctx.fillStyle = 'red';
-        /*for (let i = 0; i <= this.pocet; i++ ){
-            if (i > 0){
-                this.ae = 100 * i;
-                this.xe = 20 + this.ae;
-                ctx.fillRect(this.xe, this.ye, this.ae, this.be);
-            }else*/
-            ctx.fillRect(this.xe, this.ye, this.ae, this.be);
-      // }
-    },
-    paint2: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +110, this.ye, this.ae, this.be);
-    },
-    paint3: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +220, this.ye, this.ae, this.be);
-    },
-    paint4: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +330, this.ye, this.ae, this.be);
-    },
-    paint5: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +440, this.ye, this.ae, this.be);
-    },
-    paint6: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +550, this.ye, this.ae, this.be);
-    },
-    paint7: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +660, this.ye, this.ae, this.be);
-    },
-    paint8: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +770, this.ye, this.ae, this.be);
-    },
-    paint9: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +880, this.ye, this.ae, this.be);
-    },
-    paint10: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe, this.ye +60, this.ae, this.be);
-    },
-    paint22: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +110, this.ye +60, this.ae, this.be);
-    },
-    paint32: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +220, this.ye +60, this.ae, this.be);
-    },
-    paint42: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +330, this.ye +60, this.ae, this.be);
-    },
-    paint52: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +440, this.ye +60, this.ae, this.be);
-    },
-    paint62: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +550, this.ye +60, this.ae, this.be);
-    },
-    paint72: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +660, this.ye +60, this.ae, this.be);
-    },
-    paint82: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +770, this.ye +60, this.ae, this.be);
-    },
-    paint92: function(){
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.xe +880, this.ye +60, this.ae, this.be);
-    }
-}
+
 // ***********************************************
 let game = {
     timer: null,
+    activeBullet: null,
     play: function() {
+        //this.activeBullet = new Bullet();    
         this.timer = setInterval(function() {
             game.repaint();
         }, 20);    
@@ -113,24 +69,12 @@ let game = {
     repaint: function() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         strelec.paint();
-        enemy.paint();
-        enemy.paint2();
-        enemy.paint3();
-        enemy.paint4();
-        enemy.paint5();
-        enemy.paint6();
-        enemy.paint7();
-        enemy.paint8();
-        enemy.paint9();
-        enemy.paint10();
-        enemy.paint22();
-        enemy.paint32();
-        enemy.paint42();
-        enemy.paint52();
-        enemy.paint62();
-        enemy.paint72();
-        enemy.paint82();
-        enemy.paint92();
+        pridejEnemy();
+        bullet.paintbullet();
+        //this.game.activeBullet.bullets.forEach(function(shot){
+           
+
+       // });
     }
 }
 // **********************************************
@@ -144,8 +88,13 @@ document.addEventListener('keydown', function(event){
             break;
             
         case 'Space': 
-         ctx.fillStyle = 'blue';
-         ctx.fillRect(strelec.x +40, strelec.y - 20, 20, 20);
+        // game.activeBullet.bullets.push({x: strelec.x +40,y: strelec.y - 20,a: 20,b: 20})
+         let bullet = {
+             paintbullet: function (){
+                ctx.fillStyle = 'blue';
+                ctx.fillRect(strelec.x +40, strelec.y - 20, 20, 20);
+             }
+         }
             break;
         }
     
