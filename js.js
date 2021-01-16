14e36861beb4041f00553be7ec1d64dd5f19bc73
unknown <kaja.ricna@seznam.cz>
2021-01-16 14:23:41 +0100

    bulets + fixes

--- js.js
+++ js.js
@@ -1,111 +1,100 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-//***************************************************** 
+//*****************************************************
 class Enemy {
-    constructor(x, y, a,b){
-        this.x = x;
-        this.y = y;
-        this.a = a;
-        this.b = b;
-
-    }
-     paint (){
-
-            ctx.fillStyle = 'red';
-            ctx.fillRect(this.x, this.y, this.a, this.b);
-
-     }
+  constructor(x, y, a, b) {
+    this.x = x;
+    this.y = y;
+    this.a = a;
+    this.b = b;
+  }
+  paint() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.x, this.y, this.a, this.b);
+  }
 }
 
-let enemy1 = new Enemy(10, 10, 100, 50)
+let enemy1 = new Enemy(10, 10, 100, 50);
 
-let enemies = []
+let enemies = [];
 
-function pridejEnemy(){
-    let pomoc = 10;
-    for ( i = 0; i <= 9 ;i++ ){
-  enemies.push(new Enemy(pomoc, 10, 100, 50))
-  enemies[i].paint()
- pomoc +=110;
-    }
-
+function pridejEnemy() {
+  let pomoc = 10;
+  for (i = 0; i <= 9; i++) {
+    enemies.push(new Enemy(pomoc, 10, 100, 50));
+    enemies[i].paint();
+    pomoc += 110;
+  }
 }
 // ***********************************************
 class Bullet {
-    constructor(x, y, a,b , speed){
-        this.x = x;
-        this.y = y;
-        this.a = a; 
-        this.b = b;
-        this.speed = speed ;
-    }
+  constructor(x, y, a, b, speed) {
+    this.x = x;
+    this.y = y;
+    this.a = a;
+    this.b = b;
+    this.speed = speed;
+  }
 
-    paint () {
-        ctx.fillStyle = 'blue';
-        ctx.fillRect(this.x, this.y, this.a, this.b );
-    }
+  paint() {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(this.x, this.y, this.a, this.b);
+  }
 
-    move () {
-        this.y -= this.speed;
-    }
+  move() {
+    this.y -= this.speed;
+  }
 }
 
 let bullets = [];
 
-function pridejBullet() {
-  bullets.push(new Bullet(strelec.x, strelec.y, 10, 10,3));
-  bullets.paint();
-  bullets.move();
-}
-
 //*******************************************
 let strelec = {
-    x: canvas.width / 2,
-    y: canvas.height -50,
-    a: 100,
-    b: 50,
-    sensitivity: 10,
+  x: canvas.width / 2,
+  y: canvas.height - 50,
+  a: 100,
+  b: 50,
+  sensitivity: 10,
 
-    paint: function(){
-        ctx.fillStyle = 'black';
-        ctx.fillRect(this.x, this.y, this.a, this.b);
-    }
-
-}
+  paint: function () {
+    ctx.fillStyle = "black";
+    ctx.fillRect(this.x, this.y, this.a, this.b);
+  },
+};
 
 // ***********************************************
 let game = {
-    timer: null,
-    activeBullet: null,
-    play: function() {
-        this.timer = setInterval(function() {
-            game.repaint();
-        }, 20);    
-    },
-    repaint: function() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        strelec.paint();
-        pridejEnemy();
-        pridejBullet();
-    }
-}
-// **********************************************
-document.addEventListener('keydown', function(event){
-   
-    switch (event.code){
-       
-        case 'ArrowLeft':  strelec.x -= strelec.sensitivity;
-            break; 
-        case 'ArrowRight':  strelec.x += strelec.sensitivity;
-            break;
-            
-        case 'Space': 
-    
-            break;
-        }
-    
-    
+  timer: null,
+  play: function () {
+    this.timer = setInterval(function () {
+      game.repaint();
+    }, 20);
+  },
+
+  repaint: function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    strelec.paint();
+    pridejEnemy();
+    document.addEventListener("click", function () {
+      bullets.push(new Bullet(strelec.x + 50, strelec.y - 10, 10, 10, 3));
     });
+    for(i = 0; i  <= bullets.length; i++){
+        bullets[i].paint();
+        bullets[i].move();
+      }
+  },
+};
+// **********************************************
+document.addEventListener("keydown", function (event) {
+  switch (event.code) {
+    case "ArrowLeft":
+      strelec.x -= strelec.sensitivity;
+      break;
+    case "ArrowRight":
+      strelec.x += strelec.sensitivity;
+      break;
+  }
+});
 //****************************************************
-    game.play();
+game.play();
